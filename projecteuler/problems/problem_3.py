71a1d20c3bfd13da2f669452eefbb2cc5dd9842e
Charles Bayley <cbayley95@gmail.com>
2015-11-05 01:05:52 -0800

    Added an empty problem 3 file

--- projecteuler/problems/problem_3.py
+++ projecteuler/problems/problem_3.py
@@ -7,9 +7,12 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 
+from __future__ import print_function
+
 
 def run():
-    pass
+    value = 0
+    print('3\t: {}'.format(value))
 
 if __name__ == '__main__':
     run()
