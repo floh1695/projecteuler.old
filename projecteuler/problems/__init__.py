c1b6d3288ad6f776d728db274da9c3a4d0555cc0
Charles Bayley <cbayley95@gmail.com>
2015-11-06 00:18:33 -0800

    Added a blank problem 4

--- projecteuler/problems/__init__.py
+++ projecteuler/problems/__init__.py
@@ -3,3 +3,4 @@
 from problem_1 import run as problem_1
 from problem_2 import run as problem_2
 from problem_3 import run as problem_3
+from problem_4 import run as problem_4
---
+++ projecteuler/problems/problem_4.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2
+"""
+Largest palindrome product
+==========================
+A palindromic number reads the same both ways. The largest palindrome made
+from the product of two 2-digit numbers is 9009 = 91 * 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+from __future__ import print_function
+
+
+def run():
+    value = None
+    print('4\t: {}'.format(value))
+
+if __name__ == '__main__':
+    run()
