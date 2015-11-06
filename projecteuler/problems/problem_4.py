8328dcd6677c485a1582f5ab599bdd37ebbc75c1
Charles Bayley <cbayley95@gmail.com>
2015-11-06 00:36:04 -0800

    Whitespace change

--- projecteuler/problems/problem_4.py
+++ projecteuler/problems/problem_4.py
@@ -7,6 +7,7 @@ from the product of two 2-digit numbers is 9009 = 91 * 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
+
 from __future__ import print_function
 
 
