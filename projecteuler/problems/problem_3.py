d7954ca2b2b32834286668d6df82b2230c70de9c
Charles Bayley <cbayley95@gmail.com>
2015-11-05 23:22:42 -0800

    Solved problem 3 again

--- projecteuler/problems/problem_3.py
+++ projecteuler/problems/problem_3.py
@@ -8,10 +8,11 @@ What is the largest prime factor of the number 600851475143 ?
 """
 
 from __future__ import print_function
-
+from shared.prime import factorize
+from operator import itemgetter
 
 def run():
-    value = 0
+    value = max(factorize(600851475143))
     print('3\t: {}'.format(value))
 
 if __name__ == '__main__':
