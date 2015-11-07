c96d96287a5a78e893a35904967729d1e25a847d
Charles Bayley <cbayley95@gmail.com>
2015-11-07 15:51:54 -0800

    Solved problem 8 and added 9 to the list

--- projecteuler/problems/__init__.py
+++ projecteuler/problems/__init__.py
@@ -7,3 +7,15 @@ from problem_4 import run as problem_4
 from problem_5 import run as problem_5
 from problem_6 import run as problem_6
 from problem_7 import run as problem_7
+from problem_8 import run as problem_8
+from problem_9 import run as problem_9
+
+problems = [problem_1,
+            problem_2,
+            problem_3,
+            problem_4,
+            problem_5,
+            problem_6,
+            problem_7,
+            problem_8,
+            problem_9]
--- run.py
+++ run.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python2
 
 from __future__ import print_function
-from projecteuler import problems
+from projecteuler.problems import problems
 from time import time
 
 
 def timeit(f):
     t = time()
     f()
-    print('\t {}\n'.format(time() - t))
+    print('\t  {}'.format(time() - t))
 
 
 if __name__ == '__main__':
-    timeit(problems.problem_1)
-    timeit(problems.problem_2)
-    timeit(problems.problem_3)
-    timeit(problems.problem_4)
-    timeit(problems.problem_5)
-    timeit(problems.problem_6)
-    timeit(problems.problem_7)
+    ft = time()
+    for problem in problems:
+        timeit(problem)
+    print('\nTotal time: {}'.format(time() - ft))
