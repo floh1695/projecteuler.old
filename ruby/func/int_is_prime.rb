7cb03ebc333b60aec8cec01d5506d8ce02eadc4b
Charles <cbayley@avilela.laptop.com>
2016-10-13 16:07:27 -0400

    I removed the PrimeFactorSet class as it was pointless
    I made my prime checking ever so slightly more efficient

--- ruby/func/int_is_prime.rb
+++ ruby/func/int_is_prime.rb
@@ -1,7 +1,34 @@
 #!/usr/bin/ruby
 
+#=====================================================================
+# ** Using this for its modular exponentiation.
+#---------------------------------------------------------------------
+#  * TODO: Implement this.
+#=====================================================================
+require 'openssl'
+
 module IntegerExtIsPrime
+  #===================================================================
+  # ** Primality checker
+  #-------------------------------------------------------------------
+  #  * This is based upon the Fermat primality test,
+  #
+  #      https://en.wikipedia.org/wiki/Fermat_primality_test
+  #===================================================================
   def is_prime?
+    if self < 2
+      return false
+    elsif [2, 3].include? self
+      return true
+    end
+    if 2.to_bn.mod_exp(self - 1, self) == 1
+      return self.is_prime_check?
+    else
+      return false
+    end
+  end
+
+  def is_prime_check?
     if self < 2
         return false
     end
--- ruby/func/prime_tools.rb
+++ ruby/func/prime_tools.rb
@@ -3,18 +3,3 @@
 require_relative 'int_is_prime'
 require_relative 'int_factorize'
 
-class PrimeFactorSet
-  def initialize number
-    @number = number
-    @prime_factors = number.factorize
-  end
-
-  def largest_prime
-    @prime_factors.max
-  end
-
-  def smallest_prime
-    @prime_factor.min
-  end
-end
-
